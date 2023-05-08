3cd779aa7e63ab45628186fb7aa5892200ca514c
Elizabeth Welch-Jani <40612586+ewelch-jani@users.noreply.github.com>
2023-05-08 18:15:11 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+import setuptools
+
+setuptools.setup(
+     name="readastro",
+     version="0.1",
+     author="Elizabeth Welch-Jani",
+     author_email="elizabeth.welch-jani@yale.edu",
+     description="A python package to learn about a new astronomy topic based on abstracts from NASA ADS",
+     packages=["GetPhrases"]
+)
+
+
+
+
+
+
